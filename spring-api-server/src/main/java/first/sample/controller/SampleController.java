e1c6ba1bd1e083b00bdb958b07f84526dbd3474a
jongmin <jongmin@jongmin-PC>
2017-09-27 23:46:43 +0900

    Mybatis-Mysql 연동

--- spring-api-server/src/main/java/first/sample/controller/SampleController.java
+++ spring-api-server/src/main/java/first/sample/controller/SampleController.java
@@ -1,26 +1,35 @@
 package first.sample.controller;
 
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import first.sample.service.SampleService;
 
 @Controller
 public class SampleController {
-
-    Logger log = Logger.getLogger(this.getClass());
-     
-    @RequestMapping(value="/sample/openSampleList.do")
-    public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
-    	System.out.println("shutup");
-        ModelAndView mv = new ModelAndView("");
-        log.debug("test");
-         
-        return mv;
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="sampleService")
+	private SampleService sampleService;
+	
+	@RequestMapping(value="/sample/openSampleBoardList.do", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+    public List<Map<String,Object>> openSampleBoardList(Map<String,Object> commandMap) throws Exception{
+    	//ModelAndView mv = new ModelAndView("/sample/index");
+    	
+    	List<Map<String,Object>> list = sampleService.selectBoardList(commandMap);
+    	//mv.addObject("list", list);
+    	return list;
+    	
+    	//return mv;
     }
 }
-
-
-
