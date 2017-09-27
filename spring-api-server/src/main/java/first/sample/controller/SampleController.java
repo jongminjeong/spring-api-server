869aea74e6b9ed8acf0df8fe7b4bece7d7afbc21
jongmin <jongmin@jongmin-PC>
2017-09-27 21:52:49 +0900

    log function

---
+++ spring-api-server/src/main/java/first/sample/controller/SampleController.java
@@ -0,0 +1,26 @@
+package first.sample.controller;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SampleController {
+
+    Logger log = Logger.getLogger(this.getClass());
+     
+    @RequestMapping(value="/sample/openSampleList.do")
+    public ModelAndView openSampleList(Map<String,Object> commandMap) throws Exception{
+    	System.out.println("shutup");
+        ModelAndView mv = new ModelAndView("");
+        log.debug("test");
+         
+        return mv;
+    }
+}
+
+
+
